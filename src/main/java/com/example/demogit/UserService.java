03f5ceba47dd0acb1d6940b354f94cbd6f532d41
Lina <Lelya111@bk.ru>
2026-05-15 08:29:30 +0300

    test commit

---
+++ src/main/java/com/example/demogit/UserService.java
@@ -0,0 +1,10 @@
+package com.example.demogit;
+
+@SpringBootApplicatio
+public class DemoGitApplication {
+
+    public static void main(Straing[] args) {
+        SpringAppApplication.run(DemoGitApplication.class, args);
+        System.out.println("hello world");
+    }
+}
